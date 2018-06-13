c4c68e81c101cc16c34694626db4c6d2529ffe77
Mitchell Combs <mitch.combs.92@gmail.com>
2018-06-13 14:31:22 -0400

    Initial commit

--- RectangleTDD/UnitTest1.cs
+++ RectangleTDD/UnitTest1.cs
@@ -1,11 +1,33 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RectangleTest;
 
 namespace RectangleTDD {
     [TestClass]
     public class UnitTest1 {
+
         [TestMethod]
-        public void TestMethod1() {
+        public void TestForValidCOnstruct() {
+            var rect = new Rectangle(-1, 3);
+            Assert.IsNotInstanceOfType(rect, typeof(Rectangle));
+
+        }
+
+
+        [TestMethod]
+        public void TestArea() {
+            var length = 12;
+            var width = 20;
+            var actual = Rectangle.GetArea(length, width);
+            Assert.AreEqual(240, actual);
+        }
+
+        [TestMethod]
+        public void TestPerim() {
+            var length = 12;
+            var width = 20;
+            var actual = Rectangle.GetPerim(length, width);
+            Assert.AreEqual(64, actual);
         }
     }
 }
---
+++ RectangleTest/Rectangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleTest {
+    public class Rectangle {
+
+        public int Length { get; set; }
+        public int Width { get; set; }
+
+        public Rectangle() {
+
+        }
+
+        public Rectangle(int length, int width) {
+            Length = length;
+            Width = width;
+        }
+
+        static public int GetArea(int length, int width) {
+            var area = length * width;
+            return area;
+        }
+
+        static public int GetPerim(int length, int width) {
+            var perim = (length * 2) + (width * 2);
+            return perim;
+        }
+
+
+    }
+}
